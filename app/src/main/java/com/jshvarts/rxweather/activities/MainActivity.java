1cb9012ac89240d8d6eba66fec3684ad7fbc0b54
James Shvarts <jshvarts@gmail.com>
2017-01-15 21:33:55 -0500

    removed unused imports

--- app/src/main/java/com/jshvarts/rxweather/activities/MainActivity.java
+++ app/src/main/java/com/jshvarts/rxweather/activities/MainActivity.java
@@ -2,8 +2,6 @@ package com.jshvarts.rxweather.activities;
 
 import android.content.Intent;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
